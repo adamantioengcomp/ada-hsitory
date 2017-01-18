917ca6cf3823ae36b606b1eca86f675aa33f6577
william <william@adamantio.com.br>
2017-01-18 21:47:17 -0200

    correcao no método clear

--- ada-history.js
+++ ada-history.js
@@ -6,6 +6,46 @@ angular.module('history',[])
 			return{
 
 				/**
+				 * Navigate back to a specific page
+				 * @param {Object} params optional parameter that makes possible to
+				 *				   change/add params of the target url before goes back.
+				 *				   must be displayed as a key/value pair
+				 */
+				backTo : function(url,params){
+					status.goingBack = true;
+					for (var i in history){
+						if (history[i] == url){
+							status.current = i;
+							var prevUrl = url;
+							history = history.slice(0,Number(i)+1);
+							break;
+						}
+					}
+
+					if (prevUrl){
+						for (i in params){
+							var index = prevUrl.indexOf("?"+i+"=");
+							if (index < 0) index = prevUrl.indexOf("&"+i+"=");
+							if (index > -1){
+								var tmp = prevUrl.substr(index+1);
+								if (tmp.indexOf("&") > 0){
+									tmp = tmp.substr(0,tmp.indexOf("&"));
+								}							
+								prevUrl = prevUrl.replace(tmp,i+"="+params[i]);
+							}else{
+								if (prevUrl.indexOf("?") > -1)
+									prevUrl += "&";
+								else
+									prevUrl += "?";
+								prevUrl += (i + "=" + params[i]);
+							}
+						}
+					}
+
+        			window.location  = "#" + prevUrl;
+				},
+
+				/**
 				 * Navigate to the previous page
 				 * @param {Object} params optional parameter that makes possible to
 				 *				   change/add params of the target url before goes back.
@@ -51,7 +91,8 @@ angular.module('history',[])
 				 * Clear the history
 				 */
 				clear : function(){
-					history = [];
+					status.current = 0;
+					history.splice(0,history.length);
 				}
 			};
 
